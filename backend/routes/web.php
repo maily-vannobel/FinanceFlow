8b5dda36c4e193af6055e6db33f195b2b3106a07
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-12-05 14:40:42 +0100

    docs : create branch main, delete all files for main and update README.md to clear it

--- backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Définition des routes de l'application : chaque route est associée à un contrôleur et une méthode spécifique.
- $routes = [
-    "register" => [
-        "controller" => "RegisterController",
-        "method" => "register"
-    ],
-];
