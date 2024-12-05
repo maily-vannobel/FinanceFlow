ca2cfd57a4425b9eace22bc88d7650fa5e8aebc2
pawel-barc <pawel-barc@laplateforme.io>
2024-12-05 16:09:13 +0100

    The component Register completed, formik menages the form and potential errors

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -7,63 +7,113 @@ import axios from "axios";
 // et définition des clés pour les valeurs initiales du formulaire.
 
 const Register = () => {
-    const formik = useFormik({
-        initialValues: {
-        firstname: "",
-        lastname: "",
-        email: "",
-        password: "",
-        },
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+    },
 
-        //Définition des règles de validation pour le formulaire d'inscription
-        validationSchema: Yup.object({
-        firstname: Yup.string()
-            .min(3, "Le prenom doit contenir au moins trois caractères")
-            .required("Ce champ est requis"),
+    //Définition des règles de validation pour le formulaire d'inscription
+    validationSchema: Yup.object({
+      firstname: Yup.string()
+        .min(3, "Le prenom doit contenir au moins trois caractères")
+        .required("Ce champ est requis"),
 
-        lastname: Yup.string()
-            .min(3, "Le nom doit contenir au moins trois caractères")
-            .required("Ce champ est requis"),
+      lastname: Yup.string()
+        .min(3, "Le nom doit contenir au moins trois caractères")
+        .required("Ce champ est requis"),
 
-        email: Yup.string()
-            .email("L'email n'est pas valide")
-            .required("Ce champ est requis"),
+      email: Yup.string()
+        .email("L'email n'est pas valide")
+        .required("Ce champ est requis"),
 
-        password: Yup.string()
-            .min(8, "Le mot de passe doit contenir au moins huit caractères")
-            .matches(
-            /[A-Z]/,
-            "Le mot de passe doit contenir au moins une lettre majuscule"
-            )
-            .matches(
-            /[a-z]/,
-            "Le mot de passe doit contenir au moins une lettre minuscule"
-            )
-            .matches(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
-            .matches(
-            /[@$!%*?&]/,
-            "Le mot de passe doit contenir au moin un caractère spécial"
-            )
-            .required("Ce champ est requis"),
-        }),
-        //Envoi des données du formulaire au serveur via une requête POST, numero du port du backend: 8000, cookies dans la requête sont accepter
-        onSubmit: async (values) => {
-            try{
-                const response = await axios.post(
-                    "http://localhost:8000/register",
-                    values, {withCredientals: true}
-                );
-                alert("L'inscription a réussi");
-            } catch (error) {
-                alert("L'inscription a échoué")
-            }
-        },
-    });
+      password: Yup.string()
+        .min(8, "Le mot de passe doit contenir au moins huit caractères")
+        .matches(
+          /[A-Z]/,
+          "Le mot de passe doit contenir au moins une lettre majuscule"
+        )
+        .matches(
+          /[a-z]/,
+          "Le mot de passe doit contenir au moins une lettre minuscule"
+        )
+        .matches(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
+        .matches(
+          /[@$!%*?&]/,
+          "Le mot de passe doit contenir au moin un caractère spécial"
+        )
+        .required("Ce champ est requis"),
+    }),
+    //Envoi des données du formulaire au serveur via une requête POST, numero du port du backend: 8000, cookies dans la requête sont accepter
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/register",
+          values,
+          { withCredientals: true }
+        );
+        alert("L'inscription a réussi");
+      } catch (error) {
+        alert("L'inscription a échoué");
+      }
+    },
+  });
 
-    return (
-        
-    )
-
-
-
-}
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>
+        Prenom:
+        <input
+          type="text"
+          name="firstname"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstname}
+        />
+        {formik.touched.firstname && formik.errors.firstname ? (
+          <div>{formik.errors.firstname}</div>
+        ) : null}
+      </label>
+      <label>
+        Nom:
+        <input
+          type="text"
+          name="lastname"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastname}
+        />
+        {formik.touched.lastname && formik.errors.lastname ? (
+          <div>{formik.errors.lastname}</div>
+        ) : null}
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+      </label>
+      Le mot de passe:
+      <input
+        type="password"
+        name="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+      <button type="submit">Inscrivez-vous</button>
+    </form>
+  );
+};
