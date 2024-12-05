7b419884f5e78c60889614c597e16a5e7c0c91f6
pawel-barc <pawel-barc@laplateforme.io>
2024-12-05 12:53:09 +0100

    Starting the register option for user, new files created and the pathes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import Register from "./components/Register";
 // import "./App.css";
 
 // Composant principal de l'application
@@ -14,11 +15,15 @@ function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
           </ul>
         </nav>
         {/* Définition des routes pour l'application */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </>
