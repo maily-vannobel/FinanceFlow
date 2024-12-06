8e29f52a75aa72054730c680ca9a1ffbde0f8be2
maily-vannobel <maily.vannobel@laplateforme.io>
2024-12-06 19:16:31 +0100

    feat : add transactions routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 // Définition des routes de l'application : chaque route est associée à un contrôleur et une méthode spécifique.
- $routes = [
+$routes = [
     "register" => [
         "controller" => "RegisterController",
         "method" => "register"
@@ -9,5 +9,27 @@
     "login" => [
         "controller" => "LoginController",
         "method" => "login"
+    ],
+    
+    // CRUD transactions
+    "transactions" => [ 
+        "controller" => "TransactionsController",
+        "method" => "index"
+    ],
+    "transactions/create" => [ 
+        "controller" => "TransactionsController",
+        "method" => "store"
+    ],
+    "transactions/show" => [ 
+        "controller" => "TransactionsController",
+        "method" => "show"
+    ],
+    "transactions/update" => [
+        "controller" => "TransactionsController",
+        "method" => "update"
+    ],
+    "transactions/delete" => [ 
+        "controller" => "TransactionsController",
+        "method" => "destroy"
     ],
 ];
