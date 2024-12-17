12248b1f3f633f554b22ded814fe02d9f5aac345
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-12-17 10:32:03 +0100

    feat : add fetch for get budget data

--- frontend/src/components/Budgets.jsx
+++ frontend/src/components/Budgets.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Budgets() {
+const Budgets = () => {
+  const [budgets, setBudgets] = useState("");
+
+  const fetchBudgets = async () => {
+    try {
+      const reponse = await fetch("http://localhost:8000/read_budgets");
+
+      const jsonData = await reponse.json();
+      console.log(jsonData);
+
+      setBudgets(jsonData);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des budgets :", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBudgets();
+  }, []);
+
   return (
     <div>
       <h1>Les budgets</h1>
+
+      <ul>
+        {budgets.length > 0 ? (
+          budgets.map((budget) => <li key={budget.budget_id}>{budget.amount_limit}</li>)
+        ) : (
+          <p>Aucun budgets trouvé.</p>
+        )}
+      </ul>
     </div>
   );
-}
+};
 
 export default Budgets;
